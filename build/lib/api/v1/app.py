92560c130efda277a67f9c5ce1c9ca97fb1ce295
joseph0017 <josephogiku96@gmail.com>
2023-04-29 12:29:23 -0700

    made models into a package by using setup.py

---
+++ build/lib/api/v1/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Create and starts a Flaks application"""
+from flask import Flask, Blueprint, render_template, abort
+from ...models import storage
+from api.v1.views import app_views
+from os import getenv
+from flask import jsonify, make_response
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+print(app.url_map)
+
+@app.teardown_appcontext
+def close_database(close_db):
+    """closes database session"""
+    storage.close()
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """displays a 404 if it does not match any route"""
+    display_404 = jsonify({"error": "Not found"})
+    return make_response(display_404)
+
+
+if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", 5000)
+    app.run(host, port, threaded=True, debug=True)
---
+++ build/lib/api/v1/views/index.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""module for index.py"""
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+
+@app_views.route('/status', methods=["GET"])
+def status():
+    """returns the status of this route"""
+    return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=["GET"])
+def stats():
+    """
+    an endpoint or route that retrieves
+    the number of each objects by type
+    """
+    item_class = {
+        "amenities": storage.count("Amenity"), 
+        "cities": storage.count("City"), 
+        "places": storage.count("Place"), 
+        "reviews": storage.count("Review"), 
+        "states": storage.count("State"), 
+        "users": storage.count("User")
+    }
+    return jsonify(item_class)
