2049b45efc3f671ff8626935b210e4712887354d
joseph0017 <josephogiku96@gmail.com>
2023-05-01 09:49:58 -0700

    check_stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,17 +12,16 @@ def status():
 
 
 @app_views.route('/stats', methods=["GET"])
-def stats():
+def check_stats():
     """
     an endpoint or route that retrieves
     the number of each objects by type
     """
-    item_class = {
+    return jsonify({
         "amenities": storage.count("Amenity"),
         "cities": storage.count("City"),
         "places": storage.count("Place"),
         "reviews": storage.count("Review"),
         "states": storage.count("State"),
         "users": storage.count("User")
-    }
-    return jsonify(item_class)
+    })
