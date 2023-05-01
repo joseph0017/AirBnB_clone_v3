f39245d5e85fe519c7fa208d4d4365951e933eaf
joseph0017 <josephogiku96@gmail.com>
2023-05-01 07:04:47 -0700

    import os int

--- api/v1/app.py
+++ api/v1/app.py
@@ -30,4 +30,4 @@ def page_not_found(error):
 if __name__ == "__main__":
     api_host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
     api_port = os.environ.get('HBNB_API_PORT', 5000)
-    app.run(host=api_host, port=api_port, threaded=True, debug=True)
+    app.run(host=str(api_host), port=int(api_port), threaded=True, debug=True)
